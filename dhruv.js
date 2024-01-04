e644380e9ba589dbc61a0df54c9236479f6af51f
bajpaidhruv <dhruvbajpai97@gmail.com>
2024-01-04 16:52:56 +0530

    first commmit

---
+++ dhruv.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Frontend Devloper","DSA With Java","Web Devloper","Python",],
+    typeSpeed:60,
+    backSpeed:60,
+    backDelay:500,
+    loop:true
+})
